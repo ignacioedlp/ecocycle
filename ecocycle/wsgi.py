aa26adf5b532219559c17223a3812dc82cc53756
Ignacio Cafiero Torrubia <ignaciocafiero12@gmail.com>
2024-09-15 17:35:10 -0300

    fix name to ecocycle

--- ecocycle/wsgi.py
+++ ecocycle/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ecocyle project.
+WSGI config for ecocycle project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecocyle.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecocycle.settings')
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecocyle.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecocycle.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
