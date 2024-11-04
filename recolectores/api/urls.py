6479271abc220bcf1bf77437eaf3f80fbec295e9
Ignacio Cafiero Torrubia <ignaciocafiero12@gmail.com>
2024-11-03 21:48:01 -0300

    Limpieza

--- recolectores/api/urls.py
+++ recolectores/api/urls.py
@@ -1,21 +1,11 @@
-from recolectores.api.views.pagos_views import PagosViewSet
-from recolectores.api.views.notificacion_views import NotificacionViewSet
 from rest_framework.routers import DefaultRouter
-from recolectores.api.views.order_views import OrdenViewSet
-from recolectores.api.views.material_views import MaterialViewSet
-from recolectores.api.views.deposito_views import DepositoComunalViewSet
 from recolectores.api.views.reserva_views import ReservaViewSet, ReservaByMaterialView, CompleteReservaView, TakeReservaView
 from recolectores.api.views.auth_views import CustomTokenObtainPairView, CustomTokenRefreshView, Register
 from recolectores.api.views.fabricante_views import QueryFabricanteMaterialView, AssignFabricanteMaterialView
 from django.urls import path
 
 router = DefaultRouter()
-router.register(r'ordenes', OrdenViewSet, basename='orden')
-router.register(r'materiales', MaterialViewSet, basename='material')
-router.register(r'depositos-comunales', DepositoComunalViewSet, basename='deposito-comunal')
 router.register(r'reservas', ReservaViewSet, basename='reserva')
-router.register(r'pagos', PagosViewSet, basename='pagos')
-router.register(r'notificacion-discrepancia',NotificacionViewSet, basename='notificacion-discrepancia')
 
 urlpatterns = router.urls        
 
--- recolectores/api/views/reserva_views.py
+++ recolectores/api/views/reserva_views.py
@@ -1,22 +1,17 @@
 from rest_framework import viewsets, status
-from recolectores.models import Reserva, DepositoComunal, StockDeposito, UserMaterial
+from recolectores.models import Reserva, UserMaterial
 from recolectores.api.serializers import ReservaCreateSerializer, ReservaListSerializer, TakeReservaSerializer
 from drf_spectacular.utils import extend_schema
 from rest_framework.response import Response
-from recolectores.helpers.bonita_helpers import (
-    authenticate, getProcessId, initProcess, getTaskByCase, 
-    assignVariableByTaskAndCase, getUserIdByUsername, assignUserToTask, completeTask
-)
-from recolectores.permissions import IsAdmin, IsFabricante, IsEmpleado
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from recolectores.models import Reserva, DepositoComunal, StockDeposito
+from recolectores.models import Reserva
 from django.utils import timezone
-from rest_framework.permissions import AllowAny
 from django.utils.dateparse import parse_date
 from rest_framework.exceptions import ValidationError
 from drf_spectacular.utils import extend_schema, OpenApiParameter
 from drf_spectacular.types import OpenApiTypes
+from rest_framework.permissions import IsAuthenticated
 
 @extend_schema(
     summary="Obtener las reservas pendientes para un material",
@@ -39,11 +34,11 @@ from drf_spectacular.types import OpenApiTypes
 )
 class ReservaByMaterialView(APIView):
     # Aplica el permission_classes a nivel de clase
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticated]
 
     # No necesitas @api_view en un método dentro de una APIView
     def get(self, request, material_id):
-        reservas = Reserva.objects.filter(material__id=material_id, estado=Reserva.PENDIENTE)
+        reservas = Reserva.objects.filter(material_id=material_id, estado=Reserva.PENDIENTE)
 
         fecha_inicio = request.query_params.get('fecha_inicio')
         fecha_fin = request.query_params.get('fecha_fin')
@@ -71,7 +66,7 @@ class ReservaByMaterialView(APIView):
     tags=["Reservas"]
 )
 class TakeReservaView(APIView):
-    permission_classes = [IsEmpleado]
+    permission_classes = [IsAuthenticated]
     serializer_class = TakeReservaSerializer
 
     def post(self, request, reserva_id):
@@ -79,22 +74,19 @@ class TakeReservaView(APIView):
         Toma una reserva para un deposito en especifico enviado por el body
         """
 
-        deposito_id = request.data.get('deposito_id')
+        deposito_encargado_id = request.data.get('deposito_encargado_id')
         reserva = Reserva.objects.get(id = reserva_id)
 
         if reserva is None or reserva.estado != 'Pendiente':
             return Response({'error': 'La reserva no existe o ya fue procesada'})
 
-        if deposito_id is None:
+        if deposito_encargado_id is None:
             return Response({'error': 'Debe enviar un deposito id'})
         else:
-            deposito = DepositoComunal.objects.get(id = deposito_id)
-
-            if deposito is not None:
-                reserva.estado = Reserva.PROCESADO
-                reserva.deposito_encargado = deposito
-                reserva.save()
-                return Response({'success': 'La reserva fue tomada con exito'})
+            reserva.estado = Reserva.PROCESADO
+            reserva.deposito_encargado_id = deposito_encargado_id
+            reserva.save()
+            return Response({'success': 'La reserva fue tomada con exito'})
     
 
 @extend_schema(
@@ -103,7 +95,7 @@ class TakeReservaView(APIView):
     tags=["Reservas"]
 )
 class CompleteReservaView(APIView):
-    permission_classes = [IsEmpleado]
+    permission_classes = [IsAuthenticated]
 
     def post(self, request, reserva_id):
         """
@@ -116,15 +108,12 @@ class CompleteReservaView(APIView):
             return Response({'error': 'La reserva no existe, no fue tomada o ya fue completada'})
     
 
-        deposito = reserva.deposito_encargado
-        stock = StockDeposito.objects.filter(deposito = deposito, material = reserva.material).first()
+        deposito = reserva.deposito_encargado_id
 
-        if deposito is not None and stock is not None and stock.cantidad >= reserva.cantidad:
-            stock.cantidad -= reserva.cantidad
+        if deposito is not None:
             reserva.fecha_entrega = timezone.now()
             reserva.estado = Reserva.COMPLETADO
             reserva.save()
-            stock.save()
             return Response({'success': 'Reserva completa con exito'})
         else:
             return Response({'error': 'El deposito no tiene stock suficiente para completar la reserva'})
@@ -139,12 +128,9 @@ class ReservaViewSet(viewsets.ModelViewSet):
 
     def get_permissions(self):
         """
-        Asigna permisos basados en la acción. Por ejemplo, solo los admins pueden crear, actualizar o eliminar.
+        Asigna permisos basados en la acción. Ahora solo permite el acceso a usuarios autenticados.
         """
-        if self.action in ['destroy', 'create']:
-            permission_classes = [IsFabricante]
-        else:
-            permission_classes = [IsAdmin]
+        permission_classes = [IsAuthenticated]  # Cambiado para permitir solo usuarios autenticados
         
         return [permission() for permission in permission_classes]
 
@@ -170,17 +156,12 @@ class ReservaViewSet(viewsets.ModelViewSet):
             # Guardar la reserva en la base de datos
             reserva = serializer.save()
 
-            # Tomamos el user que esta authenticado y lo asignamos al recolector
-            reserva.user = request.user
-            reserva.save()
-
-            # Procesar la reserva en Bonita
             try:
 
-                existe = UserMaterial.objects.filter(user_id = reserva.user.id, material_id = reserva.material.id).exists()
+                existe = UserMaterial.objects.filter(proveedor_id = reserva.solicitante.id, material_id = reserva.material_id).exists()
 
                 if (not existe):
-                    UserMaterial.objects.create(user = reserva.user, material = reserva.material)
+                    UserMaterial.objects.create(proveedor = reserva.solicitante, material_id = reserva.material_id)
 
                 # self.procesar_bonita(reserva, existe)
                 return Response({
@@ -190,44 +171,6 @@ class ReservaViewSet(viewsets.ModelViewSet):
                 return Response({"detail": "Error en el proceso de Bonita"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-    def procesar_bonita(self, reserva, existe):
-        try:
-            # 1. Me autentico en Bonita
-            token, session_id = authenticate()
-
-            # 2. Obtengo el ID del proceso de la reserva
-            process_id = getProcessId(token, session_id, 'Reserva')
-
-            # 3. Instancio el proceso
-            case_id = initProcess(token, session_id, process_id)
-
-            # 4. Agrego a la reserva el caseId del proceso
-            reserva.case_bonita_id = case_id
-            reserva.save()
-
-            # 5. Busco la actividad por caso
-            task_id = getTaskByCase(token, session_id, case_id)
-
-            # 6. Asigno las variables de la actividad
-            assignVariableByTaskAndCase(token, session_id, case_id, 'user_id', reserva.user.id, 'java.lang.Integer')
-            assignVariableByTaskAndCase(token, session_id, case_id, 'primera_vez', 1 if not existe else 0, 'java.lang.Integer')
-            assignVariableByTaskAndCase(token, session_id, case_id, 'material_id', reserva.material.id, 'java.lang.Integer')
-            assignVariableByTaskAndCase(token, session_id, case_id, 'cantidad', int(reserva.cantidad), 'java.lang.Integer')
-            assignVariableByTaskAndCase(token, session_id, case_id, 'reserva_id', reserva.id, 'java.lang.Integer')
-
-            # 7. Obtengo el userId del usuario por username
-            user_id = getUserIdByUsername(token, session_id)
-
-            # 8. Asigno la actividad al usuario
-            assignUserToTask(token, session_id, task_id, user_id)
-
-            # 9. Completo la actividad asi avanza el proceso
-            completeTask(token, session_id, task_id)
-
-        except Exception as e:
-            raise Exception(f"Error en el proceso de Bonita: {str(e)}")
 
     @extend_schema(
         summary="Obtener un reserva por ID",
